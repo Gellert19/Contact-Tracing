295e9cfdb553eff11d2ca3d486d01143dc0507b4
Gellert <odixon05@gmail.com>
2024-11-05 11:35:16 +0000

    Initial commit

---
+++ contact.py
@@ -0,0 +1,13 @@
+infected = input()
+date = input()
+
+"""
+find all people at same loc on day
+filter by the infected paitient
+
+print formated list of people who need contacting
+
+one line per person
+use first reported address
+work for any date given
+"""
